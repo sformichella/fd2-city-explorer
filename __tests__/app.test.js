5a1d61a2a591a9975e6997555bbc461420b36d24
sformichella <sformichella@gmail.com>
2020-11-10 13:44:49 -0800

    munge functions tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,19 +10,42 @@ const {
   mungeReview
 } = require('../lib/utils.js');
 
-const locationTestData = require('../data/locationTestData.js');
+const { locationTestData, expectedLocationData } = require('../data/locationTestData.js');
+const { weatherTestData, expectedWeatherData } = require('../data/weatherTestData.js');
+const { trailsTestData, expectedTrailsData } = require('../data/trailsTestData.js');
+const { reviewsTestData, expectedReviewsData } = require('../data/reviewsTestData.js');
 
 describe('app routes', () => {
   describe('routes', () => {
 
     test('returns munged location data', async() => {
-      const expectation = {
-        formatted_query: 'Portland, Multnomah County, Oregon, USA',
-        latitude: '45.5202471',
-        longitude: '-122.6741949'
-      };
+      const expectation = expectedLocationData;
 
       const actual = mungeLocation(locationTestData);
+
+      expect(actual).toEqual(expectation);
+    });
+
+    test('returns munged weather data', async() => {
+      const expectation = expectedWeatherData;
+
+      const actual = mungeWeather(weatherTestData);
+
+      expect(actual).toEqual(expectation);
+    });
+
+    test('returns munged trails data', async() => {
+      const expectation = expectedTrailsData;
+
+      const actual = mungeHike(trailsTestData);
+
+      expect(actual).toEqual(expectation);
+    });
+
+    test('returns munged reviews data', async() => {
+      const expectation = expectedReviewsData;
+
+      const actual = mungeReview(reviewsTestData);
 
       expect(actual).toEqual(expectation);
     });
